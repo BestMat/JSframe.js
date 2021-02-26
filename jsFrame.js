eb9e0f87ecde5832bd5fbb2a455ab84fedb5efde
BestMat <76582849+BestMat@users.noreply.github.com>
2021-02-26 12:05:53 +0530

    Creating script

---
+++ jsFrame.js
@@ -0,0 +1,26 @@
+jsFrame([
+{
+"display": "Google",
+"url": "https://google.com"
+},
+{
+"display": "CSS Tutorial by W3schools",
+"url": "https://www.w3schools.com/css/default.asp"
+},
+{
+"display": "Tweet Button",
+"url": "https://platform.twitter.com/widgets/tweet_button.html"
+},
+{
+"display": "Twitter",
+"url": "https:/twitter"
+},
+{
+"display": "PHP Tutorial by W3Schools",
+"url": "https://www.w3schools.com/php/default.asp"
+},
+{
+"display": "VS Code",
+"url": "https://code.visualstudio.com/"
+}
+])
